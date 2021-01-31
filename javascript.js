9278da2086dea55cfc681a11a35d24a71bffc2bd
John <john_day36@yahoo.com>
2021-01-31 14:28:10 -0600

    new random number generator and added words for sentence

--- javascript.js
+++ javascript.js
@@ -1,16 +1,25 @@
-let randomNumber = Math.floor(Math.random() * 6);
-let luck = '';
-if (randomNumber === 0) {
-    luck = 'is worse than ever';
-} else if (randomNumber === 1) {
-    luck = 'is terrible';
-} else if (randomNumber === 2) {
-    luck = 'is bad';
-} else if (randomNumber === 3) {
-    luck = 'is okay';
-} else if (randomNumber === 4) {
-    luck = 'is good';
-} else if (randomNumber === 5) {
-    luck = 'is great'
+let verbs, nouns, adjectives, adverbs, preposition;
+nouns = ['cat', 'man', 'cow', 'monkey', 'baby', 'house', 'boy', 'girl'];
+verbs = ['walked', 'ran', 'cried', 'slept', 'turned', 'rolled', 'jumped', 'fell', 'flew'];
+adjectives = ['alive', 'careful', 'adventurous', 'clever', 'clean', 'courageous', 'glamorous', 'thoughtful'];
+adverbs = ['extravagantly', 'slowly', 'quickly', 'shockingly', 'calmly', 'proudly', 'daily', 'lazily'];
+preposition = ['despite', 'following', 'past', 'save', 'underneath', 'versus', 'off', 'near'];
+
+let randomGen = () => {
+    return Math.floor(Math.random() * 5);
+}
+
+let sentence = () => {
+    let rand1 = Math.floor(Math.random() * 8);
+    let rand2 = Math.floor(Math.random() * 8);
+    let rand3 = Math.floor(Math.random() * 8);
+    let rand4 = Math.floor(Math.random() * 8);
+    let rand5 = Math.floor(Math.random() * 8);
+    let rand6 = Math.floor(Math.random() * 8);
+
+    let content = "The " + adjectives[rand1] + " " + nouns[rand2] + " " + adverbs[rand3] + " " + verbs[rand4] + " because some " + nouns[rand1] + " " + adverbs[rand1] + " " + verbs[rand1] + " " + preposition[rand1] + " a " + adjectives[rand2] + " " + nouns[rand5] + " which, became a " + adjectives[rand3] + ", " + adjectives[rand4] + " " + nouns[rand6] + ".";
+
+    return content;
 };
-console.log(`On a scale of 0-5, your luck ${luck}.`);
+
+console.log(sentence());
